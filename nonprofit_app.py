77ecc5a22b5ce09013283f27c8b87072a976e514
Victory Chang <victorychang@yahoo.com>
2018-08-18 23:46:00 -0700

    Added Food Parsing, Flask, and Bootstrap.

--- nonprofit_app.py
+++ nonprofit_app.py
@@ -1,13 +1,27 @@
+
+from bs4 import BeautifulSoup
+from flask import Flask, render_template
+app = Flask(__name__)
+
 import requests
 
-nonprofit_config = [
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', nonprofits=nonprofits, food_list=food_list)
+
+
+nonprofits = [
     {
         'name': 'Second Harvest Food Bank',
         'host': 'https://www.shfb.org/',
-        'need_endpoint': '/mostneededfoods'
+        'need_endpoint': '/mostneededfoods',
+        'css_selector': 'div#colTwoContent li'
     }
 ]
 
+food_items = {}
 
 def collect_html_content(url):
     """
@@ -19,9 +33,28 @@ def collect_html_content(url):
     return response.text
 
 
+def collect_needs_from_html(html_content, css_selector):
+    """
+    Collect a list of items from html text block via a css-selector
+    :param html_content: [str] Block of html text
+    :param css_selector: [str] Corresponding CSS selector
+    """
+    beautiful_soup = BeautifulSoup(html_content, 'html.parser')
+    elements = beautiful_soup.select(css_selector)
+    return [element.get_text() for element in elements]
+
+
 if __name__ == '__main__':
-    test_data = nonprofit_config[0]
+    test_data = nonprofits[0]
+    name = test_data['name']
     host = test_data['host']
     endpoint = test_data['need_endpoint']
-    content = collect_html_content(host + endpoint)
-    print(content)
+    html_content = collect_html_content(host + endpoint)
+    print(html_content)
+
+    css_selector = test_data['css_selector']
+    food_list = collect_needs_from_html(html_content, css_selector)
+
+    print(food_list)
+
+    app.run(debug=True)
--- test/test_nonprofit_app.py
+++ test/test_nonprofit_app.py
@@ -1,11 +1,11 @@
 import responses
 
-from nonprofit_app import nonprofit_config, collect_html_content
+from nonprofit_app import nonprofit_config, collect_html_content, collect_needs_from_html
 
 
 def test_nonprofit_config_keys():
     for config in nonprofit_config:
-        assert all(key in config for key in ('name', 'host', 'need_endpoint'))
+        assert all(key in config for key in ('name', 'host', 'need_endpoint', 'css_selector'))
 
 
 @responses.activate
@@ -17,3 +17,30 @@ def test_collect_html_content():
 
     html_content = collect_html_content(sample_url)
     assert html_content == '<h1>captured html</h1>'
+
+
+def test_collect_needs_from_html():
+    sample_html = """<div id="colTwoContent"><ul>
+    <li>Canned tuna, chicken or salmon</li>
+    <li>Peanut butter</li>
+    <li>Meals in a can (soup, stew, chili)</li>
+    <li>Low-sodium canned vegetables</li>
+    <li>Canned fruit in its own juice or water</li>
+    <li>Olive or canola oil</li>
+    <li>Spices (cinnamon, chili powder, cumin, salt-free spice blends)</li>
+    <li>Canned foods with pop-top lids</li>
+    <li>Low-sugar whole grain cereals</li>
+    <li>Healthy snacks (granola bars, nuts, dried fruit)</li>
+    </ul>
+    </div>
+    """
+
+    css_selector = 'div#colTwoContent li'
+
+    collected_needs = collect_needs_from_html(sample_html, css_selector)
+    assert len(collected_needs) == 10
+    assert collected_needs[0] == 'Canned tuna, chicken or salmon'
+
+
+
+
